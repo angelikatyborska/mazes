456e9cfab4cd8c0f4bf70766ec64977b5bb39d36
Angelika Tyborska <angelikatyborska@fastmail.com>
2020-12-26 20:00:16 +0100

    Prepare deployment

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -5,7 +5,7 @@
 use Mix.Config
 
 secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
+  System.get_env("MAZES_SECRET_KEY_BASE") ||
     raise """
     environment variable SECRET_KEY_BASE is missing.
     You can generate one by calling: mix phx.gen.secret
@@ -13,7 +13,7 @@ secret_key_base =
 
 config :mazes, MazesWeb.Endpoint,
   http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
+    port: String.to_integer(System.get_env("PORT") || "5000"),
     transport_options: [socket_opts: [:inet6]]
   ],
   secret_key_base: secret_key_base
