92e7a676feefd0e89e610c6c6a6c3f80f61dd88d
Angelika Tyborska <angelikatyborska@fastmail.com>
2020-12-26 19:21:21 +0100

    Add favicon

--- lib/mazes_web/endpoint.ex
+++ lib/mazes_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule MazesWeb.Endpoint do
     at: "/",
     from: :mazes,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
